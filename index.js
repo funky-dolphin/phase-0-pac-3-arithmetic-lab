58009a3e0999e75a39895046f4ba30e8ced7c22e
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-22 15:25:37 -0500

    commit tree 4095616c5f36d095781906e619b915ca90e7b14c

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+   return a + b 
+}
+function subtract(a, b){
+return a - b
+}
+function multiply(a, b){
+return a * b 
+}
+function divide (a, b){
+return a / b
+}
+function increment (n){
+        n++
+        return n 
+}
+function decrement(n){
+    n--
+    return n 
+}
+function makeInt(n){
+    parseInt(n)
+    return n
+}
+function assumeBase(string){
+    parseInt("0")
+    return parseInt(string)
+}
+function preserveDecimal(n){
+    parseFloat(n)
+    return parseFloat(n)
+}
